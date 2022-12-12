809abdb9ec14b4eb279f2d2cc9808670c54e6af6
Floern <dev@floern.com>
2022-12-12 10:58:15 +0100

    day 6
    in short

--- 06/aoc-06.kts
+++ 06/aoc-06.kts
@@ -3,28 +3,14 @@ import java.util.LinkedList
 
 val input = File("input.txt").readLines().first().toCharArray().asList()
 
-/// Part A
-
-var resultA = 0
-val s = 4
-for (i in (s)..input.size) {
-	if (input.subList(i - s, i).toSet().size == s) {
-		resultA = i
-		break
-	}
+with("Part A") {
+	val s = 4
+	val result = input.windowed(s).indexOfFirst { it.distinct().size == s } + s
+	println("$this: $result")
 }
 
-println("A: " + resultA)
-
-/// Part B
-
-var resultB = 0
-val sB = 14
-for (i in (sB)..input.size) {
-	if (input.subList(i - sB, i).toSet().size == sB) {
-		resultB = i
-		break
-	}
+with("Part B") {
+	val s = 14
+	val result = input.windowed(s).indexOfFirst { it.distinct().size == s } + s
+	println("$this: $result")
 }
-
-println("B: " + resultB)
