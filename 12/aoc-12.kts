e921c08fb79a5084cc4f95d66f1cfd73b5da9382
Floern <dev@floern.com>
2022-12-12 10:28:54 +0100

    day 12

--- 12/aoc-12.kts
+++ 12/aoc-12.kts
@@ -2,12 +2,9 @@ import java.awt.Point
 import java.io.File
 import java.util.LinkedList
 
-val input = File("input.txt").readLines()
+val grid = File("input.txt").readLines()
 
 with("Part A") {
-	val grid = input
-	println(grid)
-
 	val start = Point(
 		grid.first { it.contains('S') }.indexOf('S'),
 		grid.indexOfFirst { it.contains('S') }
@@ -20,9 +17,9 @@ with("Part A") {
 	while (queue.isNotEmpty()) {
 		val curr = queue.keys.first()
 		val dist = queue.remove(curr)!!
-		println(curr.toString() + ": " + dist)
 
-		if (grid[curr.y][curr.x] == 'E') {
+		val currChr = grid[curr.y][curr.x]
+		if (currChr == 'E') {
 			shortestPath = dist
 			break
 		}
@@ -34,51 +31,52 @@ with("Part A") {
 			if (curr.x < grid.first().length - 1) add(Point(curr.x + 1, curr.y))
 			if (curr.y < grid.size - 1) add(Point(curr.x, curr.y + 1))
 		}
-			.filter { grid[it.y][it.x] <= grid[curr.y][curr.x] + 1 || grid[curr.y][curr.x] == 'S' || grid[it.y][it.x] == 'E' }
+			.filter {
+				val nextChr = grid[it.y][it.x]
+				nextChr <= currChr + 1 && nextChr != 'E' || currChr == 'S' || currChr >= 'y' && nextChr == 'E'
+			}
 			.filter { !visited.contains(it) && !queue.containsKey(it) }
 			.forEach { queue.put(it, dist + 1) }
 	}
 
-	println(this + ": " + shortestPath)
+	println("$this: $shortestPath")
 }
 
 
 with("Part B") {
-	val grid = input
-	println(grid)
+	val end = Point(
+		grid.first { it.contains('E') }.indexOf('E'),
+		grid.indexOfFirst { it.contains('E') }
+	)
 
-	val starts = grid.mapIndexed { y, row ->
-		row.mapIndexedNotNullTo(mutableListOf<Point>()) { x, v -> if (v == 'S' || v == 'a') Point(x, y) else null }
-	}.flatten().toList()
+	val visited = mutableSetOf<Point>()
+	val queue = linkedMapOf<Point, Int>(end to 0)
 
-	val trailLength = starts.minOf { start ->
-		val visited = mutableSetOf<Point>()
-		val queue = linkedMapOf<Point, Int>(start to 0)
+	var shortestPath = -1
+	while (queue.isNotEmpty()) {
+		val curr = queue.keys.first()
+		val dist = queue.remove(curr)!!
 
-		var shortestPath = Int.MAX_VALUE
-		while (queue.isNotEmpty()) {
-			val curr = queue.keys.first()
-			val dist = queue.remove(curr)!!
-			println(curr.toString() + ": " + dist)
-
-			if (grid[curr.y][curr.x] == 'E') {
-				shortestPath = dist
-				break
-			}
-			visited.add(curr)
-
-			buildList<Point> {
-				if (curr.x > 0) add(Point(curr.x - 1, curr.y))
-				if (curr.y > 0) add(Point(curr.x, curr.y - 1))
-				if (curr.x < grid.first().length - 1) add(Point(curr.x + 1, curr.y))
-				if (curr.y < grid.size - 1) add(Point(curr.x, curr.y + 1))
-			}
-				.filter { grid[it.y][it.x] <= grid[curr.y][curr.x] + 1 || grid[curr.y][curr.x] == 'S' || grid[it.y][it.x] == 'E' }
-				.filter { !visited.contains(it) && !queue.containsKey(it) }
-				.forEach { queue.put(it, dist + 1) }
+		val currChr = grid[curr.y][curr.x]
+		if (currChr == 'S' || currChr == 'a') {
+			shortestPath = dist
+			break
 		}
-		shortestPath
+		visited.add(curr)
+
+		buildList<Point> {
+			if (curr.x > 0) add(Point(curr.x - 1, curr.y))
+			if (curr.y > 0) add(Point(curr.x, curr.y - 1))
+			if (curr.x < grid.first().length - 1) add(Point(curr.x + 1, curr.y))
+			if (curr.y < grid.size - 1) add(Point(curr.x, curr.y + 1))
+		}
+			.filter {
+				val nextChr = grid[it.y][it.x]
+				nextChr >= currChr - 1 && currChr != 'E' || currChr == 'E' && nextChr in 'y'..'z' || currChr in 'a'..'b' && nextChr == 'S'
+			}
+			.filter { !visited.contains(it) && !queue.containsKey(it) }
+			.forEach { queue.put(it, dist + 1) }
 	}
 
-	println(this + ": " + trailLength)
+	println("$this: $shortestPath")
 }
