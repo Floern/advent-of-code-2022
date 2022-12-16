a184c02dbaf6ae9f3a6d4cf3ec717161cadf710a
Floern <dev@floern.com>
2022-12-16 15:08:59 +0100

    day 16
    second attempt

--- 16/aoc-16.kts
+++ 16/aoc-16.kts
@@ -6,251 +6,100 @@ import kotlin.math.abs
 
 val input = File("input.txt").readLines()
 
-/*
-with("Part A") {
-	val regex = """Valve (\w+) has flow rate=(\d+); tunnels? leads? to valves? (.+)""".toRegex()
-	val cave = input.map { line ->
-		regex.matchEntire(line)!!.let {
-			val room = it.groups[1]!!.value
-			val flowRate = it.groups[2]!!.value.toInt()
-			val tunnels = it.groups[3]!!.value.split(", ")
-			room to Room(flowRate, tunnels)
-		}
-	}.toMap()
-
-	fun tick(
-		minutesLeft: Int,
-		exitedRoom: String?,
-		enteredRoom: String,
-		openValves: Set<String> = emptySet(),
-		usedEdges: Set<String> = emptySet()
-	): Int {
-		val currFlowRate = openValves.sumOf { cave[it]!!.flowRate }
-		if (minutesLeft <= 1) {
-			return currFlowRate
-		}
-		if (minutesLeft > 10) {
-			println("$exitedRoom -> $enteredRoom | $minutesLeft mins: $currFlowRate released in tick")
-		}
-		if (minutesLeft < 20 && currFlowRate < 10) {
-			return -1000
-		}
-		if (minutesLeft < 10 && currFlowRate < 20) {
-			return -1000
-		}
-
-		val flowRate = cave[enteredRoom]!!.flowRate
-		val tunnels = cave[enteredRoom]!!.tunnels
-
-		return (buildList<Int> {
-			if (flowRate > 0 && enteredRoom !in openValves) {
-				add(tick(minutesLeft - 1, null, enteredRoom, openValves + enteredRoom, usedEdges))
-			}
-			if (minutesLeft == 30) { tunnels.parallelStream() } else { tunnels.stream() }
-				.forEach { nextRoom ->
-					val edge = enteredRoom + nextRoom
-					if (nextRoom != exitedRoom && edge !in usedEdges) {
-						add(tick(minutesLeft - 1, enteredRoom, nextRoom, openValves, usedEdges + edge))
-					}
-				}
-		}.maxOrNull() ?: 0) + currFlowRate
+val regex = """Valve (\w+) has flow rate=(\d+); tunnels? leads? to valves? (.+)""".toRegex()
+val cave = input.map { line ->
+	regex.matchEntire(line)!!.let {
+		val room = it.groups[1]!!.value
+		val flowRate = it.groups[2]!!.value.toInt()
+		val tunnels = it.groups[3]!!.value.split(", ")
+		room to Room(room, flowRate, tunnels)
 	}
+}.toMap()
 
-	val totalReleased = tick(30, null, "AA")
-
-	println("$this: $totalReleased")
-}*/
-
-
-with("Part B") {
-	val regex = """Valve (\w+) has flow rate=(\d+); tunnels? leads? to valves? (.+)""".toRegex()
-	val cave = input.map { line ->
-		regex.matchEntire(line)!!.let {
-			val room = it.groups[1]!!.value
-			val flowRate = it.groups[2]!!.value.toInt()
-			val tunnels = it.groups[3]!!.value.split(", ")
-			room to Room(flowRate, tunnels)
-		}
-	}.toMap()
-
-	val MINUTES = 26
-
-	val maxFlowRate = cave.map { (_, r) -> r.flowRate }.sum()
-	println("max flow rate: $maxFlowRate")
-
-	val (topValve, topValveFlowRate) = cave.maxBy { (_, r) -> r.flowRate }
-	println("topValve $topValve with flowRate $topValveFlowRate")
-
-	val visited = mutableSetOf<String>().apply { add("AA"); }
-	val queue = LinkedList<String>().apply { add("AA"); add("NEXTROUND"); }
-	var thresholdRound = 0
-	var thresholdRoundFlowRate = 0
-	var found = false
-	while (queue.isNotEmpty()) {
-		val room = queue.removeFirst()
-		if (room == "NEXTROUND") {
-			if (found) {
-				break
-			}
-			queue.add(room)
-			thresholdRound++
-		} else if (room == topValve) {
-			println("min rounds: $thresholdRound")
-			println("got to $room: ${cave!![room]}")
-			thresholdRoundFlowRate = cave[room]!!.flowRate
-			found = true
+val graph = cave.values
+	.filter { it.flowRate > 0 || it.name == "AA" }
+	.map { RoomNode(it.name, it.flowRate, mutableMapOf()) }
+graph.forEach { node ->
+	val visited = mutableSetOf<String>(node.name)
+	val depthMarker = "depthMarker"
+	val queue = LinkedList<String>().apply { add(node.name); add(depthMarker); }
+	var depth = 0
+	while (queue.size > 1) {
+		val roomName = queue.removeFirst()
+		if (roomName == depthMarker) {
+			depth++
+			queue.add(depthMarker)
 		} else {
-			cave[room]!!.tunnels.filter { it !in visited }.forEach {
+			val targetNode = graph.firstOrNull { it.name == roomName }?.takeIf { it.name != node.name }
+			if (targetNode != null) {
+				node.tunnels.put(targetNode, Weight(depth + 1, targetNode.flowRate))
+			}
+			cave[roomName]!!.tunnels.filter { it !in visited }.forEach {
 				visited.add(it)
 				queue.add(it)
 			}
 		}
 	}
-	//return@with
+}
+graph.forEach { node ->
+	println(node)
+}
+println()
 
-	fun tick(
-		minutesLeft: Int,
-		exitedRoomMe: String?,
-		enteredRoomMe: String,
-		exitedRoomPhant: String?,
-		enteredRoomPhant: String,
-		openValves: Set<String> = emptySet(),
-		usedEdgesMe: Set<String> = emptySet(),
-		usedEdgesPhant: Set<String> = emptySet()
+
+with("Part A") {
+	val MINUTES = 30
+
+	fun step(
+		currentNode: RoomNode,
+		opened: Set<String>,
+		currentFlowRate: Int = 0,
+		minutesLeft: Int = MINUTES
 	): Int {
-		val currFlowRate = openValves.sumOf { cave[it]!!.flowRate }
-		if (minutesLeft >= 25) {
-			println("$exitedRoomMe -> $enteredRoomMe && $exitedRoomPhant -> $enteredRoomPhant | $minutesLeft mins: $currFlowRate released in tick")
-		}
-		if (minutesLeft == 1 || currFlowRate == maxFlowRate) {
-			return currFlowRate * minutesLeft
-		}
-		if (minutesLeft < MINUTES - thresholdRound && currFlowRate < thresholdRoundFlowRate) {
-			return -10000
-		}
-		if (minutesLeft < 10 && currFlowRate < 40) {
-			return -10000
-		}
-
-		val flowRateMe = cave[enteredRoomMe]!!.flowRate
-		val tunnelsMe = cave[enteredRoomMe]!!.tunnels
-		val flowRatePhant = cave[enteredRoomPhant]!!.flowRate
-		val tunnelsPhant = cave[enteredRoomPhant]!!.tunnels
-
-		val options = buildList<Int> {
-			if (flowRateMe > 0 && enteredRoomMe !in openValves) {
-				// me open valve
-				if (flowRatePhant > 0 && enteredRoomPhant !in openValves && enteredRoomPhant != enteredRoomMe) {
-					// me & phant open valve
-					add(
-						tick(
-							minutesLeft - 1,
-							null,
-							enteredRoomMe,
-							null,
-							enteredRoomPhant,
-							openValves + enteredRoomMe + enteredRoomPhant,
-							usedEdgesMe,
-							usedEdgesPhant
-						)
-					)
-				} else {
-					// only me opens valve
-					tunnelsPhant.forEach { nextRoomPhant ->
-						val edgePhant = enteredRoomPhant + nextRoomPhant
-						if (nextRoomPhant != exitedRoomPhant && edgePhant !in usedEdgesPhant) {
-							add(
-								tick(
-									minutesLeft - 1,
-									null,
-									enteredRoomMe,
-									enteredRoomPhant,
-									nextRoomPhant,
-									openValves + enteredRoomMe,
-									usedEdgesMe,
-									usedEdgesPhant + edgePhant
-								)
-							)
-						}
-					}
-				}
-			} else if (flowRatePhant > 0 && enteredRoomPhant !in openValves) {
-				// only phant opens valve
-				tunnelsMe.forEach { nextRoomMe ->
-					val edgeMe = enteredRoomMe + nextRoomMe
-					if (nextRoomMe != exitedRoomMe && edgeMe !in usedEdgesMe) {
-						add(
-							tick(
-								minutesLeft - 1,
-								enteredRoomMe,
-								nextRoomMe,
-								null,
-								enteredRoomPhant,
-								openValves + enteredRoomPhant,
-								usedEdgesMe + edgeMe,
-								usedEdgesPhant
-							)
-						)
-					}
-				}
+		val results = currentNode.tunnels
+			.filter { (target, _) -> target.name !in opened }
+			.filter { (target, weight) -> weight.raw <= minutesLeft }
+			.map { (target, weight) ->
+				val remaningCumulativeFlow = step(target, opened + target.name, currentFlowRate + target.flowRate, minutesLeft - weight.raw) +
+						currentFlowRate * weight.raw
+				remaningCumulativeFlow
 			}
-
-			// nobody opens valve
-			val nextRoomCombos = mutableSetOf<Pair<String, String>>()
-			tunnelsMe.forEach { nextRoomMe ->
-				val edgeMe = enteredRoomMe + nextRoomMe
-				if (nextRoomMe != exitedRoomMe && edgeMe !in usedEdgesMe) {
-					tunnelsPhant.forEach { nextRoomPhant ->
-						val edgePhant = enteredRoomPhant + nextRoomPhant
-						if (nextRoomPhant != exitedRoomPhant && edgePhant !in usedEdgesPhant) {
-							val edgeComboA = Pair(nextRoomMe, nextRoomPhant)
-							val edgeComboB = Pair(nextRoomPhant, nextRoomMe)
-							if (enteredRoomMe != enteredRoomPhant || (edgeComboA !in nextRoomCombos && edgeComboB !in nextRoomCombos)) {
-								nextRoomCombos += edgeComboA
-							}
-						}
-					}
-				}
-			}
-			val completed = AtomicInteger()
-			if (minutesLeft >= MINUTES - 2) {
-				nextRoomCombos.parallelStream()
-			} else {
-				nextRoomCombos.stream()
-			}
-				.forEach { (nextRoomMe, nextRoomPhant) ->
-					val edgeMe = enteredRoomMe + nextRoomMe
-					val edgePhant = enteredRoomPhant + nextRoomPhant
-					add(
-						tick(
-							minutesLeft - 1,
-							enteredRoomMe,
-							nextRoomMe,
-							enteredRoomPhant,
-							nextRoomPhant,
-							openValves,
-							usedEdgesMe + edgeMe,
-							usedEdgesPhant + edgePhant
-						)
-					)
-					if (minutesLeft == MINUTES) {
-						val total = nextRoomCombos.size
-						val c = completed.incrementAndGet()
-						println("finished $c/$total: $enteredRoomMe -> $nextRoomMe && $enteredRoomPhant -> $nextRoomPhant")
-					}
-				}
-		}
-		val collectionFlowRate = options.filterNotNull().maxOrNull() ?: 0
-		return collectionFlowRate + currFlowRate
+		val bestRemaningCumulativeFlow = results.maxOrNull() ?: (minutesLeft * currentFlowRate)
+		return bestRemaningCumulativeFlow
 	}
 
-	val totalReleased = tick(MINUTES, null, "AA", null, "AA")
+	var startNode = graph.first { it.name == "AA" }
+	val cumulativeFlow = step(startNode, setOf(startNode.name))
 
-	println("$this: $totalReleased")
+	println("$this: $cumulativeFlow")
+}
+
+
+with("Part B") {
+
 }
 
 
 data class Room(
+	val name: String,
 	val flowRate: Int,
 	val tunnels: List<String>,
 )
+
+data class RoomNode(
+	val name: String,
+	val flowRate: Int,
+	val tunnels: MutableMap<RoomNode, Weight>,
+) {
+	override fun equals(other: Any?): Boolean = (other as? RoomNode)?.name == name
+	override fun hashCode(): Int = name.hashCode()
+	override fun toString(): String {
+		return "$name ($flowRate) -> " + tunnels.map { (n, w) -> "${w.raw}>" + n.name }.sorted().joinToString()
+	}
+}
+
+data class Weight(val raw: Int, val targetFlowRate: Int) {
+	fun adjusted(minutesRemaining: Int): Int {
+		return raw - targetFlowRate * (minutesRemaining - raw)
+	}
+}
